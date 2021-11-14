ed2f2e55b6c5dedc16b2cf77751daebe0503d30f
mrburke00 <debu7497@colorado.edu>
2021-11-14 15:28:33 -0700

    bugs

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #snakemake -s /scratch/Shares/layer/workspace/devin_sra/sv_step/sv_pipe/src/genotype.smk --dag | dot -Tpdf > dag2.pdf
-#snakemake -s /scratch/Shares/layer/workspace/devin_sra/sv_step/sv_pipe/src/multisample.smk \
-#          --cores 18 \
-#	  --until AllCall \
-#          --resources disk_mb=700000 \
-#          --use-conda --conda-frontend mamba \
-#          --conda-prefix /scratch/Shares/layer/workspace/devin_sra/sv_step/sv_results/snakemake-conda \
-#          --scheduler greedy
+
+snakemake -s /scratch/Shares/layer/workspace/devin_sra/sv_step/sv_pipe/src/multisample.smk \
+          --cores 16 \
+	  --until AllCall \
+          --resources disk_mb=700000 \
+          --use-conda --conda-frontend mamba \
+          --conda-prefix /scratch/Shares/layer/workspace/devin_sra/sv_step/sv_results/snakemake-conda \
+          --scheduler greedy
+
 #snakemake -s /scratch/Shares/layer/workspace/devin_sra/sv_step/sv_pipe/src/genotype.smk \
 #          --cores 16 \
 #          --resources disk_mb=700000 \
