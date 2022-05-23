5ef9a059f62f5d7655495368e388af9626283a9c
mrburke00 <46036921+mrburke00@users.noreply.github.com>
2022-05-23 12:34:10 -0600

    Delete align_src directory

--- align_src/spawn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sra_file="/scratch/Shares/layer/workspace/devin_sra/sv_step/pigs.txt"
-#sra_file="pigs.txt"
-procs=4
-lines=$(wc -l $sra_file | cut -d " " -f 1)
-x=$((lines / procs))
-cat $sra_file | gargs -log my.log --n $x -p $procs "sbatch align.sbatch {}"
-
